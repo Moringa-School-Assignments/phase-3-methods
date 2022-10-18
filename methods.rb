29e6200a9f65530add275b32c61c0af9f6c68b4b
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-10-18 17:38:32 +0300

    complete assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    if name == 'Naureen'
+        puts 'Hello, Naureen!'
+    elsif name == 'Jimmy'
+        puts 'Hello, Jimmy!'
+   end
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(one, two)
+    return one + two
+end
+
+def halve(num)
+   if num.is_a? Integer
+    return num / 2
+   else
+    return nil
+   end
+end
+
+
